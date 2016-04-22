b6e32b674e9f3e12b8adeaed19ff48edaee883fd
Jitae Kim <originerd@gmail.com>
2016-04-22 18:40:26 +0900

    Fit image into inherited space with flex

--- FitImage.js
+++ FitImage.js
@@ -2,22 +2,57 @@ import React, {
   Component,
   Dimensions,
   Image,
+  PropTypes,
 } from 'react-native';
 
-class FitImage extends Component {
-  _getSize() {
-    const width = Dimensions.get('window').width;
-    const ratio = width / this.props.originalWidth;
-    const height = this.props.originalHeight * ratio;
+const propTypes = {
+  originalHeight: PropTypes.number,
+  originalWidth: PropTypes.number,
+  source: PropTypes.object.isRequired,
+  style: PropTypes.number,
+};
 
-    return { height, width };
+class FitImage extends Component {
+  constructor(props) {
+    super(props);
+
+    const height = 0;
+
+    this.state = {
+      height,
+    };
+  }
+
+  _getRatio(width) {
+    return width / this.props.originalWidth;
+  }
+
+  _getHeight(width) {
+    return this.props.originalHeight * this._getRatio(width);
+  }
+
+  _setHeight(event) {
+    var { width } = event.nativeEvent.layout;
+
+    const height = this._getHeight(width);
+
+    this.setState({
+      height,
+    });
   }
 
   render() {
     return (
       <Image
         source={this.props.source}
-        style={[this._getSize(), this.props.style]}
+        style={[
+          {
+            flex: 1,
+            height: this.state.height,
+          },
+          this.props.style
+        ]}
+        onLayout={(event) => this._setHeight(event)}
       />
     );
   }
