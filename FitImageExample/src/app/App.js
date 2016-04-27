bb2680a1924ee6b7b82c6586f39ceb370acbf1c8
Jitae Kim <originerd@gmail.com>
2016-04-28 00:10:27 +0900

    Modify View to ScrollView and add style none image

--- FitImageExample/src/app/App.js
+++ FitImageExample/src/app/App.js
@@ -1,33 +1,29 @@
 import React, {
   Component,
+  ScrollView,
   StyleSheet,
-  View
 } from 'react-native';
 
 import FitImage from 'react-native-fit-image';
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#F5FCFF',
+    flex: 1,
+    paddingTop: 20,
   },
-  fitImage: {
-    borderRadius: 20,
-  }
 });
 
 function App() {
   return (
-    <View style={styles.container}>
+    <ScrollView style={styles.container}>
+      { /* Style none */ }
       <FitImage
         source={{uri: 'http://facebook.github.io/react/img/logo_og.png'}}
         originalWidth={400}
         originalHeight={400}
-        style={styles.fitImage}
       />
-    </View>
+    </ScrollView>
   );
 }
 
