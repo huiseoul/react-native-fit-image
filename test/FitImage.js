37167a4ce9bdd563af04581f48bb0d6d45d5bd8e
Jitae Kim <originerd@gmail.com>
2016-05-27 18:12:02 +0900

    Add tests for initializing of FitImage component

--- test/FitImage.js
+++ test/FitImage.js
@@ -8,13 +8,39 @@ import { shallow } from 'enzyme';
 import FitImage from '../src/FitImage';
 
 test('FitImage', nest => {
-  const setup = () => {
+  const setup = (props = {}) => {
     const source = { uri: 'http://facebook.github.io/react/img/logo_og.png' };
-    const props = { source };
+    const defaultProps = { source };
+
+    props = { ...props, ...defaultProps }
     const wrapper = shallow(<FitImage {...props} />);
 
     return { wrapper, props };
   };
+
+  nest.test('...throw error on initializing', assert => {
+    assert.throws(
+      () => { setup({ width: 100 }); }, Error,
+      'when width props exist without height'
+    );
+
+    assert.throws(
+      () => { setup({ height: 100 }); }, Error,
+      'when height props exist without width'
+    );
+
+    assert.throws(
+      () => { setup({ originalWidth: 100 }); }, Error,
+      'when originalWidth props exist without originalHeight'
+    );
+
+    assert.throws(
+      () => { setup({ originalHeight: 100 }); }, Error,
+      'when originalHeight props exist without originalWidth'
+    );
+
+    assert.end();
+  });
 
   nest.test('...render', assert => {
     const { wrapper } = setup();
